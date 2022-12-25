a895e4d65940a81d860926ca0b06840ca1fb5ff5
Leo <leo.zedek@insa-rouen.fr>
2022-12-25 14:42:44 +0100

    Page de statistiques de la salle

--- backend/routers/stats_salon.js
+++ backend/routers/stats_salon.js
@@ -15,6 +15,8 @@ router.post('/', (req, res) => {
 	let data = req.body;
 	let id_canva = data["id"];
 
+	const colors = [];
+
 	if (id_canva != null) {
 
 		db.serialize(() => {
@@ -39,6 +41,18 @@ router.post('/', (req, res) => {
 			})
 
 			statement.finalize();
+
+/*			db.all("SELECT * FROM colors;", (err, rows) => {
+            if (err) {
+                next(err);
+            } else {
+                for (row in rows) {
+                    colors.push({ color: rows[row].colorCode, id: rows[row].id - 1 });
+                }
+            }
+            res.json(colors);
+        });*/
+
 		})
 	}
 
@@ -51,9 +65,20 @@ router.post('/', (req, res) => {
 // Seulement pour tester, à retirer plus tard
 
 router.get('/', (req, res) => {
-
 	let data = req.query;
 	let id_canva = data["id"];
+	
+	res.render("stats_salon.ejs", {id_canva : id_canva});
+
+})
+
+router.post('/get_statistics', (req, res) => {
+	let data = req.body;
+	let id_canva = data["id"];
+
+	let canva_name, color_stats, nb_modif;
+	const colors = {};
+
 
 	if (id_canva != null) {
 
@@ -66,15 +91,13 @@ router.get('/', (req, res) => {
 				}
 				else {
 					if (result != null) {
-						let canva_name = result["name"];
-						let color_stats = result["colorStats"];
-						let nb_modif = result["nbModif"];
+						canva_name = result["name"];
+						color_stats = result["colorStats"];
+						nb_modif = result["nbModif"];
 
 						console.log(canva_name);
 						console.log(color_stats);
 						console.log(nb_modif);
-
-						res.render("stats_salon.ejs", {canva_name : canva_name, color_stats : color_stats, nb_modif : nb_modif});
 					}
 					else {
 						res.status(406).end('<p>Wrong credentials! <a href="/"><button>Try again!</button></a></p>');
@@ -83,13 +106,26 @@ router.get('/', (req, res) => {
 			})
 
 			statement.finalize();
+
+			db.all("SELECT * FROM colors;", (err, rows) => {
+	            if (err) {
+	                next(err);
+	            } else {
+	                for (row in rows) {
+	                    colors[rows[row].id] = rows[row].colorCode;
+	                }
+	            }
+	            console.log({colors : colors, canva_name : canva_name, color_stats : color_stats, nb_modif : nb_modif});
+				res.json({colors : colors, canva_name : canva_name, color_stats : color_stats, nb_modif : nb_modif});
+		
+
+	        });
 		})
 	}
 
 	else {
-		console.err("id_canva pas valable");
+		console.log("id_canva pas valable");
 	}
-
 })
 
 module.exports = router;
