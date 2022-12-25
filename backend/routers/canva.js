36027be1af20ec7c852ac499a64bbcc60b04a1b7
mathias <mathias.hersent@insa-rouen.fr>
2022-12-25 21:05:50 +0100

    marche toujours pas mais plus propre

--- backend/routers/canva.js
+++ backend/routers/canva.js
@@ -3,6 +3,18 @@ const router = express.Router();
 const sqlite3 = require('sqlite3').verbose();
 const jimp = require('jimp');
 
+// const WebSocket = require('ws');
+// const app = require('../server');
+// const server = app.listen(3000);
+// const wss = new WebSocket.Server({ server });
+
+// wss.on('connection', function connection(ws) {
+//   // send the current hour to the client every second
+//   setInterval(function() {
+//     ws.send(new Date().toTimeString());
+//   }, 1000);
+// });
+
 // connecting an existing database (handling errors)
 const db = new sqlite3.Database('./db/test.sqlite', (err) => {
     if (err) {
@@ -12,6 +24,30 @@ const db = new sqlite3.Database('./db/test.sqlite', (err) => {
 });
 
 let lastModifs = [];
+
+// router.get('/websocket', (req, res, next) => {
+//     // create a WebSocket server
+//     console.log("get");
+//     const wss = new WebSocket.Server({ noServer: true });
+  
+//     wss.on('connection', function connection(ws) {
+//         // ws.on('message', function incoming(message) {
+//         //     console.log('received: %s', message);
+//         // });
+//         console.log("connection");
+//         setInterval(function() {
+//             ws.send(JSON.stringify(lastModifs));
+//         }, 1000);
+    
+//         // ws.send('something');
+//     });
+  
+//     // upgrade the HTTP connection to a WebSocket connection
+//     req.upgrade(wss, (ws) => {
+//       // WebSocket connection established
+//     });
+//     next();
+// });
 
 // Mathias Hersent
 router.post('/info', function (req, res) {
@@ -58,9 +94,9 @@ router.post('/update', function (req, res) {
             image.scan(0, 0, image.bitmap.width, image.bitmap.height, function (x, y, idx) {
                 if (x == data.x && y == data.y) {
                     console.log(idx);
-                    this.bitmap.data[idx + 0] = 255; // red channel
-                    this.bitmap.data[idx + 1] = 0;   // green channel
-                    this.bitmap.data[idx + 2] = 0;   // blue channel
+                    this.bitmap.data[idx + 0] = data.color[0]; // red channel
+                    this.bitmap.data[idx + 1] = data.color[1];   // green channel
+                    this.bitmap.data[idx + 2] = data.color[2];   // blue channel
                 }
             });
             return image;
@@ -76,10 +112,10 @@ router.post('/update', function (req, res) {
         });
 });
 
-router.get('/', (req, res, next) => {
-    console.log("coucou");
-    res.status(200);
-    next();
+router.post('/getlastmodifs', function (req, res) {
+    let data = req.body;
+
+    res.json(JSON.stringify(lastModifs));
 });
 
 router.use('/', (req, res) => {
